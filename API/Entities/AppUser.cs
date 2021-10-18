f296f25d33f4ac9c5be83c59a8b61163b9c29ea4
Drake Reporto <82571014+dreporto@users.noreply.github.com>
2021-10-18 10:39:32 +0200

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,5 +5,7 @@ namespace API.Entities
         // --properties --
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
